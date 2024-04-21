a8b80baec0e6fdd682d0e830526e99f236f05152
niharpalem <74230650+niharpalem@users.noreply.github.com>
2024-04-20 21:08:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-import cv2
-import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 
-def main():
-    st.title("Webcam Stream (Using OpenCV)")
-
-    # Video capture setup (replace 0 with your camera index if needed)
-    cap = cv2.VideoCapture(0)
-
-    # Streamlit app loop
-    while True:
-        ret, frame = cap.read()
-
-        # Display the webcam frame in the app
-        st.image(frame, channels="BGR")
-
-        # Exit loop if 'q' key is pressed
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    # Release capture resources
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+webrtc_streamer(key="sample")
